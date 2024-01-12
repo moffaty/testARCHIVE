ebe6bd3dee2c4e818f2831a3def3545cbc3d5c02
Шестаков Егор Павлович <sep@mns.mnsspb.ru>
2024-01-12 15:51:37 +0300

    feat(login CI)

--- app.js
+++ app.js
@@ -1,12 +1,80 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const jwt = require('jsonwebtoken');
 
-const main_dir = '/main_dir'
+// middleware
+const checkHeadersMiddleware = (req, res, next) => {
+    const requiredHeaders = ['authorization', 'Content-Type'];
+    console.log(req.headers);
+    for (const header of requiredHeaders) {
+      if (!req.headers[header]) {
+        return res.status(400).json({ error: `Missing: ${header}` });
+      }
+    }
+    next();
+  };
+
+// token
+const token = "zxc";
+
+// hash
+const main_dir = '/main_dir';
+const hashLenght = 10;
+
+// db
+const hostDB = 'srv-archive';
+const userDB = 'agp';
+const passwordDB = 'Lmx00R@FMPaA';
+
+/**
+ * Connects to a MySQL database.
+ * @param {string} dbNAME The name of the database to connect to.
+ * @param {Object} res The *response* from server query. Need for response error 
+ * @returns {Object} The MySQL connection object.
+ */
+function connectToMySQL(dbNAME, res) {
+    const connection = mysql.createConnection({
+        host: hostDB,
+        user: userDB,
+        password: passwordDB,
+        database: dbNAME,
+        port: 3306
+    });
+    connection.connect((err) => {
+        if (err) { 
+            console.dir(err); 
+            res.json({  error:'Error connection to DB' }); 
+        }
+    });
+    return connection;
+}
+
+function getUserData(req, res) {
+    if (!req.body.password && req.body.username) {
+        res.json({ error:`Error: haven't login or password` });
+    } 
+    const username = req.body.username;
+    const password = req.body.password;
+    const data = [username, password];
+    return data;
+}
+
+function isSQLResponseHaveError(error, res) {
+    if (error) {
+        console.log(error);
+        res.json({ error: error.sqlMessage });
+        return true;
+    }
+    return false;
+}
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // для обработки URL-кодированных данных
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
@@ -44,6 +112,59 @@ app.post('/get-dir-info', async (req, res) => {
     }
 });
 
+app.post('/register-user', async (req, res) => {
+    const data = getUserData(req, res);
+    const connection = connectToMySQL('personals', res);
+    const query = 'INSERT INTO auth(username, password) VALUES(?, ?)';
+    connection.query(query, data, (error, result) => {
+        if (!isSQLResponseHaveError(error, res)) {
+            res.json({ response: `Registered new user: ${data[0]}!` })
+        }
+    })
+})
+
+app.post('/delete-user', (req, res) => {
+    const data = getUserData(req, res);
+    const connection = connectToMySQL('personals', res);
+    const query = 'DELETE FROM auth WHERE username = ? AND password = ?';
+    connection.query(query, data, (error, result) => {
+        if (!isSQLResponseHaveError(error, res)) {
+            if (result.affectedRows > 0) {
+                res.json({ response: `Deleted user: "${username}"!` });
+            } else {
+                res.json({ response: "User doesn't exist" });
+            }
+        }
+    })
+})
+
+app.post('/login', (req, res) => {
+    const data = getUserData(req, res);
+    const connection = connectToMySQL('personals', res);
+    const query = 'SELECT * FROM auth WHERE username = ? AND password = ?';
+    connection.query(query, data, (error, result) => {
+        if (!isSQLResponseHaveError(error, res)) {
+            const username = data[0];
+            const userToken = jwt.sign({ username }, token, { expiresIn: '0.5h' });
+            res.status(200).send(`\nToken: ${userToken}\nUse token in header to get acces. Like this: "Authorization: Bearer token"`);
+        }
+    })
+})
+
+app.post('/secret', checkHeadersMiddleware, (req, res) => {
+    res.send('hola)');
+})
+
 app.listen(3100, () => {
     console.log('Started http://localhost:3100')
 })
+
+async function test()  {
+    const userP = '1234';
+    const registerP = await bcrypt.hash(userP, hashLenght);
+    console.log("IN BD:", registerP);
+    const loginP = await bcrypt.compare(userP, registerP);
+    console.log("LOGIN:",loginP);
+}
+test();
+// console.dir(bcrypt.compare(rawPassword, user.password));
